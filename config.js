4f3886267bf6c3027a839e521656dc12b3c0fedf
saheenshoukath <saheenshoukath@gmail.com>
2022-06-04 17:20:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,7 @@
 import * as firebase from 'firebase'
 
 const FIREBASE_CONFIG  = {
-    apiKey: "AIzaSyBcFoN87oFcOvw5cVsnOBM0pj5oNq6yqM8",
-    authDomain: "projectx-7c40c.firebaseapp.com",
-    databaseURL: "https://projectx-7c40c.firebaseio.com",
-    projectId: "projectx-7c40c",
-    storageBucket: "projectx-7c40c.appspot.com",
-    messagingSenderId: "790112231693",
-    appId: "1:790112231693:web:b13c00dc5315314dcf9044",
-    measurementId: "G-ZV5QJ723QV"
+    //config
   }
 
   const Firebase = firebase.initializeApp(FIREBASE_CONFIG)
